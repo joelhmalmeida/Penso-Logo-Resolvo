9f4bdc2884515b70cb88c427b9e61e0acb71fd7f
Joel Almeida <83190972+joelhmalmeida@users.noreply.github.com>
2022-11-09 20:39:40 +0000

    Variaveis de cada Tipo

---
+++ script.js
@@ -0,0 +1,16 @@
+const myName = "Joel";
+console.log(myName);
+
+let myAge = 38;
+console.log(myAge);
+
+let isMyAgeReal = True;
+console.log(isMyAgeReal);
+
+var myCountry = "Portugal";
+console.log(myCountry);
+
+const myFatherName = "Julio";
+console.log(myFatherName);
+
+
